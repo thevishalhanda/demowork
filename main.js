d46c35cc42626447ecd88bd571fd6ece7f409616
thevishalhanda <vishalhanda0101@gmail.com>
2022-01-07 13:28:01 +0530

    lastchange

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let p = 2000;
 let q = "evelated"
 let w = "toxic"
 let e = "desire"
-
-
+let f = "hasan"
+let baseline = "EFFECT THAT CHNAGE LIFE OF PEOPLE"
 console.log(x+y+z);
-console.log(q+w+e);
+console.log(q+w+e+f);
+console.log(baseline)
